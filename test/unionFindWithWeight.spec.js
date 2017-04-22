0daff237d067f4aa3841d05366d99cb817a27ad0
Yevhenii <honcharenko.zhenya@gmail.com>
2017-04-22 10:34:20 +0300

    add unionFindWithWeight and tests

--- test/unionFindWithWeight.spec.js
+++ test/unionFindWithWeight.spec.js
@@ -3,91 +3,101 @@
 const algolib = require('../index.js');
 
 describe.only('#unionFindWithWeight', function() {
+
 	it('should create proper datastructure', function() {
-		let uf = new algolib.unionFindWithWeight();
-		uf.connect.should.be.instanceOf(Function);
-		uf.isConnected.should.be.instanceOf(Function);
-		uf.count.should.be.instanceOf(Function);
-		uf.component.should.be.instanceOf(Function);
+		let uFWW = new algolib.unionFindWithWeight();
+		uFWW.connect.should.be.instanceOf(Function);
+		uFWW.isConnected.should.be.instanceOf(Function);
+		uFWW.count.should.be.instanceOf(Function);
+		uFWW.component.should.be.instanceOf(Function);
 	});
 
 	it('should #isConnected return false if not connected', function() {
-		let uf = new algolib.unionFindWithWeight();
-		let isElementConnected = uf.isConnected(3,4);
+		let uFWW = new algolib.unionFindWithWeight();
+		let isElementConnected = uFWW.isConnected(3,4);
 		isElementConnected.should.be.false();
 	})
 
 	it('should #isConnected return true if elements are connected', function() {
-		let uf = new algolib.unionFindWithWeight();
-		uf.connect(1,4);
-		let isElementConnected  = uf.isConnected(1,4);
+		let uFWW = new algolib.unionFindWithWeight();
+		uFWW.connect(1,4);
+		let isElementConnected  = uFWW.isConnected(1,4);
 		isElementConnected.should.be.true();
 	})
 
-	it('should #count return amount of components', function() {
-		let uf = new algolib.unionFindWithWeight();
-		uf.connect(1,2);
-		let countComp  = uf.count();
-		countComp.should.be.eql(1);
+	it('should #count return count of elements (4)', function() {
+		let uFWW = new algolib.unionFindWithWeight();
+		uFWW.connect(1,4);
+		uFWW.connect(2,3);
+		let count  = uFWW.count();
+		count.should.be.eql(4);
 	})
 
-	it('should #count return amount of components', function() {
-		let uf = new algolib.unionFindWithWeight();
-		uf.connect(1,2);
-		uf.connect(2,3);
-		let countComp  = uf.count();
-		countComp.should.be.eql(1);
-	})
+	// it('should #count return amount of components', function() {
+	// 	let uFWW = new algolib.unionFindWithWeight();
+	// 	uFWW.connect(1,2);
+	// 	let countComp  = uFWW.count();
+	// 	countComp.should.be.eql(1);
+	// })
 
-	it('should #count return amount of components', function() {
-		let uf = new algolib.unionFindWithWeight();
-		uf.connect(1,2);
-		uf.connect(2,3);
-		uf.connect(0,4);
-		uf.connect(5,0);
-		let countComp  = uf.count();
-		countComp.should.be.eql(2);
-	})
+	// it('should #count return amount of components', function() {
+	// 	let uFWW = new algolib.unionFindWithWeight();
+	// 	uFWW.connect(1,2);
+	// 	uFWW.connect(2,3);
+	// 	let countComp  = uFWW.count();
+	// 	countComp.should.be.eql(1);
+	// })
 
-	it('should #count return amount of components', function() {
-		let uf = new algolib.unionFindWithWeight();
-		let countComp  = uf.count();
-		countComp.should.be.eql(0);
-	})
+	// it('should #count return amount of components', function() {
+	// 	let uFWW = new algolib.unionFindWithWeight();
+	// 	uFWW.connect(1,2);
+	// 	uFWW.connect(2,3);
+	// 	uFWW.connect(0,4);
+	// 	uFWW.connect(5,0);
+	// 	let countComp  = uFWW.count();
+	// 	countComp.should.be.eql(2);
+	// })
 
-	it('should #component(a) return component', function() {
-		let uf = new algolib.unionFindWithWeight();
-		uf.connect(1,2);
-		uf.connect(2,3);
-		uf.connect(0,4);
-		uf.connect(5,0);
-		uf.connect(7, 0);
-		uf.connect(8, 9);
-		let component  = uf.component(4);
-		component.should.be.eql(4);
-	})
+	// it('should #count return amount of components', function() {
+	// 	let uFWW = new algolib.unionFindWithWeight();
+	// 	let countComp  = uFWW.count();
+	// 	countComp.should.be.eql(0);
+	// })
 
-	it('should #component(a) return component', function() {
-		let uf = new algolib.unionFindWithWeight();
-		uf.connect(1,2);
-		uf.connect(2,3);
-		uf.connect(0,4);
-		uf.connect(5,0);
-		uf.connect(7, 0);
-		uf.connect(8, 9);
-		let component  = uf.component(1);
-		component.should.be.eql(3);
-	})
+	// it('should #component(a) return component', function() {
+	// 	let uFWW = new algolib.unionFindWithWeight();
+	// 	uFWW.connect(1,2);
+	// 	uFWW.connect(2,3);
+	// 	uFWW.connect(0,4);
+	// 	uFWW.connect(5,0);
+	// 	uFWW.connect(7, 0);
+	// 	uFWW.connect(8, 9);
+	// 	let component  = uFWW.component(4);
+	// 	component.should.be.eql(4);
+	// })
 
-	it('should #component(a) return component', function() {
-		let uf = new algolib.unionFindWithWeight();
-		uf.connect(1,2);
-		uf.connect(2,3);
-		uf.connect(0,4);
-		uf.connect(5,0);
-		uf.connect(7, 0);
-		uf.connect(8, 9);
-		let component  = uf.component(10);
-		(typeof component).should.be.equal('undefined');
-	})
+	// it('should #component(a) return component', function() {
+	// 	let uFWW = new algolib.unionFindWithWeight();
+	// 	uFWW.connect(1,2);
+	// 	uFWW.connect(2,3);
+	// 	uFWW.connect(0,4);
+	// 	uFWW.connect(5,0);
+	// 	uFWW.connect(7, 0);
+	// 	uFWW.connect(8, 9);
+	// 	let component  = uFWW.component(1);
+	// 	component.should.be.eql(3);
+	// })
+
+	// it('should #component(a) return component', function() {
+	// 	let uFWW = new algolib.unionFindWithWeight();
+	// 	uFWW.connect(1,2);
+	// 	uFWW.connect(2,3);
+	// 	uFWW.connect(0,4);
+	// 	uFWW.connect(5,0);
+	// 	uFWW.connect(7, 0);
+	// 	uFWW.connect(8, 9);
+	// 	let component  = uFWW.component(10);
+	// 	(typeof component).should.be.equal('undefined');
+	// })
+
 })
